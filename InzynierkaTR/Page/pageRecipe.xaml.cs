0bbf5faeb2bb6b05df284daf3f4e003570a27a3e
Kondorian <tomekredmerski@gmail.com>
2022-11-17 19:27:15 +0100

    Create TODO for recipe page

--- InzynierkaTR/Page/pageRecipe.xaml.cs
+++ InzynierkaTR/Page/pageRecipe.xaml.cs
@@ -24,5 +24,49 @@ namespace InzynierkaTR.Page
         {
             InitializeComponent();
         }
+
+        /* TODO: Implement functions to save Images + add favorite bool
+        void AddImages()
+        {
+            var files = Directory.GetFiles(System.IO.Path.Combine(root, "Images\\Rozne\\Imprezy"), "*.*");
+
+            foreach (var file in files)
+            {
+                ImagePath id = new ImagePath()
+                {
+                    Path = file,
+                    FileName = System.IO.Path.GetFileName(file),
+
+                };
+                images.Add(id);
+            }
+
+
+        }
+
+        void CopyImages()
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image jpeg(*.jpg)|*.jpg|Image png(*.png)|*.png";
+            ofd.DefaultExt = ".jpeg";
+
+            // Process open file dialog box results 
+            if (ofd.ShowDialog() == true)
+            {
+                var filename = ofd.FileName; // Get the filename from absolute path
+                var file = System.IO.Path.GetFullPath(ofd.FileName);
+
+                var filename2 = file.Substring(file.LastIndexOf("\\") + 1);
+                string _finalPath = System.IO.Path.Combine(root, "Images\\Rozne\\Imprezy").ToString(); //it is the destination folder path e.g,C:\Users\Neha\Pictures\11-03-2014
+                if (Directory.Exists(_finalPath))
+                {
+
+                    _finalPath = System.IO.Path.Combine(_finalPath, filename2);
+
+                    System.IO.File.Copy(file, _finalPath, true);
+                }
+
+            }
+        }*/
     }
 }
---
+++ README.txt
@@ -0,0 +1,4 @@
+Tytuł: Aplikacja wspomagająca dobór przepisu kulinarnego na podstawie posiadanych składników spożywczych
+Title: An application supporting the selection of a recipe based on the available food ingredients
+
+Status(17.11.2022): Created ui for main menu, prepared todo for the rest of pages
