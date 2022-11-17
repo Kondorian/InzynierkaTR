1c7582af66ad199744414705238676baf4d4cf28
Kondorian <tomekredmerski@gmail.com>
2022-11-17 16:21:44 +0100

    Create a page *correctly*

--- InzynierkaTR/Page/startPage.xaml.cs
+++ InzynierkaTR/Page/startPage.xaml.cs
@@ -12,20 +12,17 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using InzynierkaTR.Page;
 
-namespace InzynierkaTR
+namespace InzynierkaTR.Page
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for startPage.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class startPage
     {
-        public MainWindow()
+        public startPage()
         {
             InitializeComponent();
-            Main.Content = new startPage();
         }
-
     }
 }
