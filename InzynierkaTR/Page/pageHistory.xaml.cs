adfc2f6319f82784916b1ecb2cf2811d6326f23b
Kondorian <tomekredmerski@gmail.com>
2023-01-20 16:08:23 +0100

    Created pageHistory template

--- InzynierkaTR/Page/pageHistory.xaml.cs
+++ InzynierkaTR/Page/pageHistory.xaml.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,7 +13,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace InzynierkaTR.Page
@@ -20,9 +22,43 @@ namespace InzynierkaTR.Page
     /// </summary>
     public partial class pageHistory
     {
+        string root = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
         public pageHistory()
         {
             InitializeComponent();
         }
+
+        void CopyImages()
+        {
+            var ofd = new OpenFileDialog();
+            ofd.Filter = "Image jpeg(*.jpg)|*.jpg|Image png(*.png)|*.png";
+            ofd.DefaultExt = ".jpeg";
+
+            // Process open file dialog box results 
+            if (ofd.ShowDialog() == true)
+            {
+                var filename = ofd.FileName;
+                var file = System.IO.Path.GetFullPath(ofd.FileName);
+
+                var filename2 = file.Substring(file.LastIndexOf("\\") + 1);
+                string _finalPath = System.IO.Path.Combine(root, "Images\\Rozne\\Imprezy").ToString();
+                if (Directory.Exists(_finalPath))
+                {
+
+                    _finalPath = System.IO.Path.Combine(_finalPath, filename2);
+
+                    System.IO.File.Copy(file, _finalPath, true);
+                }
+
+            }
+        }
+
+        private void buttonBack_Click(object sender, RoutedEventArgs e)
+        {
+            startPage startPage = new startPage();
+            this.NavigationService.Navigate(startPage);
+        }
+        private void buttonBack_MouseEnter(object sender, MouseEventArgs e) { buttonBackText.FontStyle = FontStyles.Italic; }
+        private void buttonBack_MouseLeave(object sender, MouseEventArgs e) { buttonBackText.FontStyle = FontStyles.Normal; }
     }
 }
