daccfd77e147baf725dca5f571ed100b9e8c89eb
Kondorian <tomekredmerski@gmail.com>
2023-01-26 00:01:51 +0100

    Recipe selection by ingridient done

--- InzynierkaTR/Page/pageIngridient.xaml.cs
+++ InzynierkaTR/Page/pageIngridient.xaml.cs
@@ -27,6 +27,8 @@ namespace InzynierkaTR.Page
         List<string> ingridientNames = new List<string>();
         List<ImagePath> images = new List<ImagePath>();
         string root = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location);
+        private string recipeTitle { get; set; }
+
         public pageIngridient()
         {
             InitializeComponent();
@@ -59,7 +61,103 @@ namespace InzynierkaTR.Page
 
         private void buttonSearch_Click(object sender, RoutedEventArgs e)
         {
+            string[] igs = new string[ingridients];
+            List<bool> bl = new List<bool>();
+            List<string> recipeList = new List<string>();
+            int i = 0;
 
+            foreach (var ingridient in stackPanelRecipe.Children.OfType<ComboBox>())
+            {
+                if (ingridient.Text != "")
+                {
+                    igs[i] = ingridient.Text;
+                }
+                i++;
+            }
+
+            var files = Directory.GetFiles(System.IO.Path.Combine(root, "Images"), "*.*")
+                .Where(s => s.EndsWith(".txt"));
+            i=0;
+
+            foreach (var file in files) // Checking if has ingridient
+            {
+                bl.Add(false);
+                using (StreamReader textFile = new StreamReader(file))
+                {
+                    string ln;
+
+                    while ((ln = textFile.ReadLine()) != null)
+                    {
+                        if (ln == "") { break; }
+                        int length = ln.IndexOf("|");
+                        ln = ln.Substring(0, length);
+                        for (int j = 0; j < ingridients; j++)
+                        {
+                            if (ln == igs[j])
+                            {
+                                bl[i] = true;
+                            }
+                        }
+                        if (bl[i]) { break; }
+
+                    }
+                    if (bl[i])
+                    {
+                        recipeTitle = fileName(file);
+                        string path = root + "\\Images\\" + recipeTitle + ".png";
+                        if (!File.Exists(path))
+                        {
+                            path = root + "\\Images\\" + recipeTitle + ".jpg";
+                            if (!File.Exists(path))
+                            {
+                                break;
+                            }
+                            else
+                            {
+                                addFile(path);
+                            }
+                        }
+                        else
+                        {
+                            addFile(path);
+                        }
+                        
+                    }
+                    i++;
+                    textFile.Close();
+                }
+            }
+            /*
+            var files = Directory.GetFiles(System.IO.Path.Combine(root, "Images"), "*.*")
+                .Where(s => s.EndsWith(".jpg") || s.EndsWith(".png"));
+
+            foreach (var file in files)
+            {
+                ImagePath id = new ImagePath()
+                {
+                    Path = file,
+                    FileName = fileName(file),
+                };
+                images.Add(id);
+            }*/
+            ImageList.ItemsSource = images;
+        }
+
+        string fileName(string file)
+        {
+            string fileName = System.IO.Path.GetFileName(file);
+            fileName = fileName.Substring(0, fileName.IndexOf("."));
+            return fileName;
+        }
+
+        void addFile(string file)
+        {
+            ImagePath id = new ImagePath()
+            {
+                Path = file,
+                FileName = fileName(file),
+            };
+            images.Add(id);
         }
 
         private void ImageList_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -69,7 +167,7 @@ namespace InzynierkaTR.Page
             TextBox textBox = GetVisualChild<TextBox>(lbi);
             if (textBox != null)
                 textBox.Focus();
-            //recipeTitle = textBox.Text;
+            recipeTitle = textBox.Text;
         }
 
         private T GetVisualChild<T>(Visual parent) where T : Visual
@@ -97,6 +195,12 @@ namespace InzynierkaTR.Page
             startPage startPage = new startPage();
             this.NavigationService.Navigate(startPage);
         }
+        private void Image_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            pageDetails pageDetails = new pageDetails(recipeTitle);
+            this.NavigationService.Navigate(pageDetails);
+        }
+
         private void buttonBack_MouseEnter(object sender, MouseEventArgs e) { buttonBackText.FontStyle = FontStyles.Italic; }
         private void buttonBack_MouseLeave(object sender, MouseEventArgs e) { buttonBackText.FontStyle = FontStyles.Normal; }
 
